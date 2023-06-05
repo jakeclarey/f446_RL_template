621718fc53bbe5f01cf4124a028678b9dfd194c0
jakeclarey <jakeclarey@gmail.com>
2023-06-04 20:57:23 -0400

    Created a blink example template with tasks inc.

---
+++ Core/Src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stm32f446xx.h>
+
+void sysTick_init(void);
+void ms_delay(int n);
+
+int main(void) {
+  sysTick_init();
+  RCC->AHB1ENR |= 1;
+  GPIOA->MODER &= ~3 << (2 * 5U);
+  GPIOA->MODER |= 1 << (2 * 5U);
+  while (1) {
+    GPIOA->ODR |= 1 << 5U;
+    ms_delay(1000);
+    GPIOA->ODR &= ~1 << 5U;
+    ms_delay(1000);
+  } // end of while (1)
+} // end of main(void)
+
+void sysTick_init(void) {
+  SysTick->CTRL = 0;          // disable SysTick while configuring
+  SysTick->LOAD = 0x00FFFFFF; // load max value
+  SysTick->VAL = 0; // clears current value register by writing anything to it
+  SysTick->CTRL = 0x00000005; // enable SysTick
+}
+
+void ms_delay(int n) {
+  SysTick->LOAD = ((n * 16000) - 1); // load ms value for 16Mhz clock
+  SysTick->VAL = 0;                  // clear current value register
+  while ((SysTick->CTRL & 0x00010000) == 0)
+    ; // wait until COUNTFLAG is set
+}
