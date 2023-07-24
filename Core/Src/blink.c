b27a8b826166b0b67e6b735ecd140e2da37b43ea
Jake <jakeclarey@gmail.com>
2023-07-24 16:23:22 -0400

    formatting

--- Core/Src/blink.c
+++ Core/Src/blink.c
@@ -1,20 +1,23 @@
 #include "blink.h"
 
-void doBlink(void) {
+void doBlink(void)
+{
   GPIOA->ODR |= 1 << 5U;
   ms_delay(1000);
   GPIOA->ODR &= ~1 << 5U;
   ms_delay(1000);
 }
 
-void sysTick_init(void) {
+void sysTick_init(void)
+{
   SysTick->CTRL = 0;          // disable SysTick while configuring
   SysTick->LOAD = 0x00FFFFFF; // load max value
-  SysTick->VAL = 0; // clears current value register by writing anything to it
+  SysTick->VAL = 0;           // clears current value register by writing anything to it
   SysTick->CTRL = 0x00000005; // enable SysTick
 }
 
-void ms_delay(int n) {
+void ms_delay(int n)
+{
   SysTick->LOAD = ((n * 16000) - 1); // load ms value for 16Mhz clock
   SysTick->VAL = 0;                  // clear current value register
   while ((SysTick->CTRL & 0x00010000) == 0)
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stm32f446xx.h>
 
-int main(void) {
+int main(void)
+{
   sysTick_init();
   RCC->AHB1ENR |= 1;
   GPIOA->MODER &= ~3 << (2 * 5U);
   GPIOA->MODER |= 1 << (2 * 5U);
-  while (1) {
+  while (1)
+  {
     doBlink();
-  } // end of while (1)
+  } // end of while(1)
 } // end of main(void)
